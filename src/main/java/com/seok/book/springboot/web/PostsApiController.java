6d91dc49bb20a40d651cec2093b1aef795e13d29
seok <nova9353@gmail.com>
2020-12-02 11:21:12 +0900

    PostApiUpdateTest

--- src/main/java/com/seok/book/springboot/web/PostsApiController.java
+++ src/main/java/com/seok/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.seok.book.springboot.web;
 
 import com.seok.book.springboot.service.posts.PostsService;
+import com.seok.book.springboot.web.dto.PostsResponseDto;
 import com.seok.book.springboot.web.dto.PostsSaveRequestDto;
+import com.seok.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor                        //final이 선언된 모든 필드를 인자값6으로 하는 생성자를 만들어 줌 -> lombok을 쓰지 않으면 의존성 관계가 변경될 때마다 생성자 코드를 계속 변경해야 함
 @RestController
@@ -16,5 +16,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestsDto){
+        return postsService.update(id, requestsDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
