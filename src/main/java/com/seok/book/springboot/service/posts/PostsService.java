de15509dbb791680e0ba2a1e0cd01c9fd51ac636
seok <nova9353@gmail.com>
2020-12-14 13:54:06 +0900

    add update/delete

--- src/main/java/com/seok/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/seok/book/springboot/service/posts/PostsService.java
@@ -26,7 +26,7 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+                new IllegalArgumentException("No Article No.id = " + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
@@ -35,9 +35,16 @@ public class PostsService {
 
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+                new IllegalArgumentException("No Article No. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No Article No.id = " +id));
+
+        postsRepository.delete(posts);
     }
 
     @Transactional(readOnly = true)
--- src/main/java/com/seok/book/springboot/web/IndexController.java
+++ src/main/java/com/seok/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.seok.book.springboot.web;
 
 import com.seok.book.springboot.service.posts.PostsService;
+import com.seok.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;                    //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있슴
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
